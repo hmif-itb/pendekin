8e0be4f7b240c500d500ad40128c9d119325283f
donbasta <faddilafarras@gmail.com>
2021-09-26 22:56:09 +0700

    feat: add typing animation

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -1,46 +1,61 @@
 <section>
-  <pre>
-    <code style="color: #6c99bb">
-      {"const "}
-    </code>
-    <code style="color: #9e86c8"> 
-      {"url"}
-    </code>
-    <code style="color: #ff3e00">
-      {" = `"}
-    </code>
-    <code style="color: #e5b567">
-      hmif.link/
-    </code>
-    <code style="color: #ff3e00">
-      {"${"}    
-    </code>
-    <code style="color: white">
-      route
-    </code>
-    <code style="color: #ff3e00">
-      {"}`"}
-    </code>
-  </pre>
+  <div class="code">
+    <span style="color: #6c99bb; margin-right: 1ch">
+      {'const'}
+    </span>
+    <span style="color: #9e86c8; margin-right: 1ch">
+      {'url'}
+    </span>
+    <span style="color: #ff3e00">
+      {'= `'}
+    </span>
+    <span style="color: #e5b567">
+      {'hmif.link/'}
+    </span>
+    <span style="color: #ff3e00">
+      {'${'}
+    </span>
+    <span style="color: #ffffff">
+      {'route'}
+    </span>
+    <span style="color: #ff3e00">
+      {'}`'}
+    </span>
+  </div>
 </section>
 
 <style>
   section {
     text-align: center;
-    padding: 1em;
     margin: 100px 0;
     width: 100%;
     height: 400px;
     background-color: black;
-  }
-  code {
-    font-size: 7em;
-    font-weight: 100;
-  }
-  pre {
     display: flex;
     align-items: center;
     justify-content: center;
-    margin-top: 100px;
+  }
+  .code {
+    display: flex;
+    font-size: 7em;
+    font-weight: 100;
+    font-family: monospace;
+    width: 32ch;
+    animation: typing 3s steps(32), blink 0.5s step-end infinite alternate;
+    white-space: nowrap;
+    overflow: hidden;
+    border-right: 3px solid;
+  }
+
+  @keyframes typing {
+    from {
+      width: 0;
+    }
+  }
+
+  @keyframes blink {
+    50% {
+      border-color: white;
+    }
   }
 </style>
